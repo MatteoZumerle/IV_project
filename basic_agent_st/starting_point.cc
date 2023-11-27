ae44a999d6f0e635166c4692866a8c41d8fe3f58
MatteoZumerle <matteo.zumerle@virgilio.it>
2023-11-27 22:56:07 +0100

    Started the trlght main

--- basic_agent_st/starting_point.cc
+++ basic_agent_st/starting_point.cc
@@ -18,6 +18,7 @@
 #include <vector>
 #include <algorithm>
 
+
 extern "C" {
 #include "screen_print_c.h"
 }
@@ -86,14 +87,14 @@ int main(int argc, const char * argv[]) {
 
             // vehicle state
             
-            double vmin, vmax, tmin, tmax;
+            double tmin, tmax;
             double coeffsT1[6];
             double coeffsT2[6];          
             double Tf;
 
             // Second set composed by the best parameters calculated
             
-            double bestCoeffs[6];
+           
             double bestV;
             double bestS;
             double bestT;
@@ -101,66 +102,109 @@ int main(int argc, const char * argv[]) {
             double minTime = 10;             // Minimum time required to pass
             double maxTime = 35;             // Maximum time required to pass
 
-            // Define the boundaries in terms of acceleration and velocities
+            // Define the boundaries in terms of acceleration and velocities -------- FROM HERE THE CODES OF THE SLIDES PAG 104
             
             double minAcc = -20;
             double maxAcc = 10;
-            double acc = fmin(fmax(in->ALgtFild, minAcc), maxAcc);  // Here the saturation of the possible values of acceleration INVERTED WRT THE PRESENTED!
-            double vel = in->VLgtFild;                              // Extract from the simulation the actual speed of the car
+            double a0 = fmin(fmax(in->ALgtFild, minAcc), maxAcc);  // Here the saturation of the possible values of acceleration INVERTED WRT THE PRESENTED!
+            double v0 = in->VLgtFild;                              // Extract from the simulation the actual speed of the car
             static double initial_dist = in->TrfLightDist;          // Extract from the simulation the initial dist from traffic light
 
+            double lookahead = fmax(50, v0*5);                      // Look ahead distance to consider building the primitives
+            double vmin = 3;
+            double vmax = 15;
+            double vr = in->RequestedCruisingSpeed;
+
+            double xs = 5;                // Safety space (in order to stop before)
+            double Ts = xs/vmin;          // Safety time in order to stop before
+            double xin = 10;              // Width of the intersection where is present the traffic light
+            double Tin = xin/vmin;
+
+            double xtr;
+            double xstop;
+            double mstar[6];
+            double mstar2[6];
+            double xf = in->TrfLightDist; 
+
+            double m1[6], m2[6];
+
+            double Tgreen;
+            double Tred;
+
+            double s_max;
+            double tf;
             // Save the initial time and distance of the TL from the starting point
 
             static double ECUupTimeOld = 0;
             ECUupTimeOld = in->ECUupTime;
 
-            double Vr = in->RequestedCruisingSpeed;         // ?
-            double sf = in->TrfLightDist;                   // extract from the simulation time after time the distance from the tl
-            double s_max;
-            
             //---------------------------------------------------------------------------//
            
-            //Build a first dummy condition to generate the primitives 
-            //-> when passed the lenght of 80m stops the car
-            double stop_point = sf - 80; // Stop the car 80 meters before the traffic lights
-            if(initial_dist - in->TrfLightDist > stop_point){
+            if(in->NrTrfLights !=0){
+               xtr = in->TrfLightDist;
+               xstop = (in->TrfLightDist) -xs/2;
+            }
                 
-                // Activate the stopping primitive
-                StoppingPrimitive(vel, acc, stop_point, coeffsT1, &s_max, &Tf);    // Check how to pass the variables
-                
-
-            }else{
-            
-                // Activate the passing primitive
-                
-                vmin = 1;
-                vmax = 50;
-                tmin = minTime - (in->ECUupTime);                                       // Rescale wrt the CPU timing
-                tmax = maxTime - (in->ECUupTime);
-                PassingPrimitive(acc, vel, stop_point, vmin, vmax, tmin, tmax, coeffsT1, coeffsT2); // Check how to pass the variables
+            if(in->NrTrfLights == 0 || xtr >= lookahead){
+                PassingPrimitive(a0, v0, lookahead, vr, vr, 0, 0, mstar, mstar2);
+            }
+            else{
+                switch (in->TrfLightCurrState) {
+                    case 1:
+                        Tgreen = 0;
+                        Tred = in->TrfLightFirstTimeToChange - Tin;
+                        break;
+                    case 2:
+                        Tgreen = in->TrfLightSecondTimeToChange + Ts;
+                        Tred = in->TrfLightThirdTimeToChange - Tin;
+                        break;
+                    case 3:
+                        Tgreen = in->TrfLightFirstTimeToChange + Ts;
+                        Tred = in->TrfLightSecondTimeToChange - Tin;
+                        break;
             }
 
+            if (in->TrfLightCurrState == 1 && in->TrfLightDist<= xs){
+                PassingPrimitive(a0, v0, lookahead, vr, vr, 0, 0, mstar, mstar2);
+            }
+            else{
+                PassingPrimitive(a0, v0, xtr, vmin, vmax, Tgreen, Tred, m1, m2);
+            }
+            if(m1 == 0 && m2==0){
+                StoppingPrimitive(v0, a0, xtr, mstar, &s_max, &tf);
+            }
+            else{
+                if( (m1[3]<0 && m2[3]>0) || (m1[3]>0 && m2[3]<0) ){
+                    PassingPrimitivej0(a0, v0, xtr, vmin, vmax, mstar);
+                }
+                else if (abs(m1[3])<0 && abs(m2[3])>0)
+                {
+                    for (int i = 0; i < sizeof(m1); i++) {
+                        mstar[i] = m1[i];
+                    }
+                }
+                else {
+                    for (int i = 0; i < sizeof(m2); i++) {
+                        mstar[i] = m2[i];
+                    }
+                }
+
+                
+            }
+
+            // -- RICONTROLLARE LA SCALETTA DEGLI IF CHE NON SONO CONVINTO...
 
 
-            bestCoeffs[0] = coeffsT1[0];            // Assign the founded values in function of the action that will be selected
-            bestCoeffs[1] = coeffsT1[1];
-            bestCoeffs[2] = coeffsT1[2];
-            bestCoeffs[3] = coeffsT1[3];
-            bestCoeffs[4] = coeffsT1[4];
-            bestCoeffs[5] = coeffsT1[5];
-            bestS = sf;                             
-            bestV = vel;
-            bestT = Tf;
-
+           
             
             // Build the jerk controller
 
             
             static double old_req_acc = 0;
             double longGain = 1;
-            double jerk0 = bestCoeffs[3];           // Collect the values of the coefficients in order to build the controller
-            double snap0 = bestCoeffs[4];
-            double crakle0 = bestCoeffs[5];
+            double jerk0 = mstar[3];           // Collect the values of the coefficients in order to build the controller
+            double snap0 = mstar[4];
+            double crakle0 = mstar[5];
 
             double t_offset = 0.0;                  // Generate the time required to the calculation that generates a delay
 
@@ -168,13 +212,13 @@ int main(int argc, const char * argv[]) {
             double jTnew = jerk0 + (DT + t_offset) * snap0 + 0.5 * pow(DT + t_offset, 2) * crakle0;  // Final jerk after the deltaT
             double request_acc = fmin(fmax(old_req_acc + longGain * (DT * (jTnew+jTold) * 0.5), minAcc), maxAcc);
             old_req_acc = request_acc;
-            double req_vel = v_opt_fun(DT, vel, acc, bestS, bestV, 0.0, bestT);     // CORREEGERE CHE NON VA!
+            double req_vel = v_opt_fun(DT, v0, a0, bestS, bestV, 0.0, bestT);     // CORREEGERE CHE NON VA!
            
             // PID configuration
 
             double P_gain = 0.02;
             double I_gain = 1;
-            double Error = request_acc - acc;
+            double Error = request_acc - a0;
             static double integration = 0;
             integration = integration + Error * DT;  // Missing to define the accelerations the error and the integration step
           
@@ -188,7 +232,7 @@ int main(int argc, const char * argv[]) {
             logger.log_var("PrimitivePlot", "dist", 160 - in-> TrfLightDist);
             logger.log_var("PrimitivePlot", "vel", in->VLgtFild);
             logger.log_var("PrimitivePlot", "acc", in->ALgtFild);
-            logger.log_var("PrimitivePlot", "Sf", sf);
+            logger.log_var("PrimitivePlot", "Sf", xf);
             logger.log_var("PrimitivePlot", "vmin", vmin);
             logger.log_var("PrimitivePlot", "vmax", vmax);
             logger.log_var("PrimitivePlot", "tmin", tmin);
@@ -196,12 +240,12 @@ int main(int argc, const char * argv[]) {
             logger.log_var("PrimitivePlot", "req_vel", req_vel);
             logger.log_var("PrimitivePlot", "req_acc", request_acc);
             logger.log_var("PrimitivePlot", "T", bestT);
-            logger.log_var("PrimitivePlot", "C0", bestCoeffs[0]);
-            logger.log_var("PrimitivePlot", "C1", bestCoeffs[1]);
-            logger.log_var("PrimitivePlot", "C2", bestCoeffs[2]);
-            logger.log_var("PrimitivePlot", "C3", bestCoeffs[3]);
-            logger.log_var("PrimitivePlot", "C4", bestCoeffs[4]);
-            logger.log_var("PrimitivePlot", "C5", bestCoeffs[5]);
+            logger.log_var("PrimitivePlot", "C0", mstar[0]);
+            logger.log_var("PrimitivePlot", "C1", mstar[1]);
+            logger.log_var("PrimitivePlot", "C2", mstar[2]);
+            logger.log_var("PrimitivePlot", "C3", mstar[3]);
+            logger.log_var("PrimitivePlot", "C4", mstar[4]);
+            logger.log_var("PrimitivePlot", "C5", mstar[5]);
             logger.log_var("PrimitivePlot", "Fasullo", in->CycleNumber);
 
             // Write log
@@ -215,7 +259,7 @@ int main(int argc, const char * argv[]) {
             printLogVar(message_id, "TF dist [m]", in->TrfLightDist);
             printLogVar(message_id, "LongVel", in->VLgtFild);
             printLogVar(message_id, "LongAcc", in->ALgtFild); //acc
-            printLogVar(message_id, "Speed", vel);
+            printLogVar(message_id, "Speed", v0);
             printLogVar(message_id, "Req vel", req_vel);
             printLogVar(message_id, "Req pedal", req_pedal);
 
